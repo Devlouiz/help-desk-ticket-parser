1909444c86584eb9cc1700d994734028514af4b6
devlouis <louisonyedi@gmail.com>
2025-07-31 22:03:13 +0100

    Checking ticket resolution time

--- ticket_parser.py
+++ ticket_parser.py
@@ -3,21 +3,38 @@ from collections import defaultdict
 
 category_counts = defaultdict(int)
 tag_counts = defaultdict(int) #tag counts
+resolution_counts = defaultdict(int)
+
+# Classify ticket resolution speed
+def classify_resolution_speed(hours):
+
+    if hours <= 1.5:
+        return "Fast"
+    elif hours <= 6.0:
+        return "Normal"
+    else:
+        return "Slow"
 
 with open('C:/Users/Ddr3/Documents/CyberSecurity/help desk ticket parser and reporter/intercom_report_csv.csv',
           'r') as file:
     read = csv.DictReader(file)
+
 
     for row in read:
         category = row['Status']
         category_counts[category] += 1
 
         tags = row['Tags']
-        if tags: #if ticket contains tags
+        if tags: #check if not empty
             individual_tag = tags.split(',')
             for tag in individual_tag:
                 if tag:
                     tag_counts[tag] += 1
+
+        if row['Resolution Time (hours)']: #check if not empty
+            hours = float(row['Resolution Time (hours)'])
+            specific_resolution_speed = classify_resolution_speed(hours)
+            resolution_counts[hours] += 1
 
 # Print a report
 print("Ticket Summary")
